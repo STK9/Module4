732b55ef6af453e68361034bda18afc03bdf5058
Patrick <SU_TOW_KWANG01.PT@ICHAT.SP.EDU.SG>
2020-11-17 16:39:11 +0800

    v1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
+// *******************************
+// START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
+// *******************************
+
+(function(){
+	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+	var i=0;
+	for (i=0; i<names.length; i++){
+	  	if (names[i].charAt(0).toLowerCase() =="j" ){	 	
+	 		byeSpeaker.speak(names[i]);
+		  } else {
+		  	helloSpeaker.speak(names[i]);
+	  		}
+	}
+})();
+
+
+
 
